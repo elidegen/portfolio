c5580dea482625c8c5ab5f401d792d4485c3d3c7
elidegen <elidegen@icloud.com>
2023-10-14 11:19:40 +0200

    different components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 import { ScrollButtonComponent } from './scroll-button/scroll-button.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectComponentComponent } from './project-component/project-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     StartScreenComponent,
-    ScrollButtonComponent
+    ScrollButtonComponent,
+    AboutMeComponent,
+    ContactComponent,
+    FooterComponent,
+    PortfolioComponent,
+    SkillsComponent,
+    ProjectComponentComponent
   ],
   imports: [
     BrowserModule,

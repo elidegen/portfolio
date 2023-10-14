5ba6defcd372bf64795a3ada54c66432088d9bf7
elidegen <elidegen@icloud.com>
2023-10-14 10:15:21 +0200

    starting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { ScrollButtonComponent } from './scroll-button/scroll-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StartScreenComponent,
+    ScrollButtonComponent
   ],
   imports: [
     BrowserModule,

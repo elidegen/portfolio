bc09014a40ef0f5a28b0ef8cb873d6dfdf85dde7
elidegen <elidegen@icloud.com>
2023-10-18 23:55:06 +0200

    projects design game laptop img

--- src/app/portfolio/project-component/project-component.component.ts
+++ src/app/portfolio/project-component/project-component.component.ts
@@ -22,7 +22,7 @@ export class ProjectComponentComponent {
       description: 'Are you ready to prove your bravery and determination to save the kingdom from darkness? Follow in the footsteps of a true knight and experience an exciting adventure in Knight\'s Journey. Become the hero the kingdom desperately needs and defeat the dark forces that threaten it!',
       github: 'https://github.com/elidegen/game',
       ftp: 'https://elijah-degen.developerakademie.net/game/',
-      img: 'assets/img/laptop.png'
+      img: 'assets/img/laptopGame.png'
     },
     {
       name: 'Join',

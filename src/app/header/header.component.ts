6080069188241e003bcd441658d551215a119b2a
elidegen <elidegen@icloud.com>
2023-11-04 15:24:31 +0100

    mobile header menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,30 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   scrollPosition: number = 0;
+  innerWidth: any;
+  menuShow: boolean = false;
+
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+  }
 
   @HostListener("window:scroll", ['$event'])
   onWindowScroll(event: Event): void {
     this.scrollPosition = window.scrollY;
     // console.log(this.scrollPosition);
-    
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.innerWidth = window.innerWidth;
+  }
+
+  toggleMenu() {
+    this.menuShow = !this.menuShow;
   }
 }

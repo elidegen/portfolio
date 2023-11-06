436641a06444299eb3060168c8b0641a7d8065ad
elidegen <elidegen@icloud.com>
2023-11-06 20:52:48 +0100

    menu bug fix

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,28 +6,32 @@ import { Component, HostListener, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  scrollPosition: number = 0;
   innerWidth: any;
+  touched: boolean = false;
   menuShow: boolean = false;
 
   ngOnInit() {
     this.innerWidth = window.innerWidth;
   }
 
-  @HostListener("window:scroll", ['$event'])
-  onWindowScroll(event: Event): void {
-    this.scrollPosition = window.scrollY;
-  }
-  
   @HostListener('window:resize', ['$event'])
-  onResize(event: Event) {
+  onResize() {
     this.innerWidth = window.innerWidth;
-    if(this.innerWidth > 670) {
+    if (this.innerWidth > 670) {
       this.menuShow = false
     }
   }
 
+  @HostListener('document:click', ['$event'])
+  checkClick(event: Event) {
+    const clickedElement = event.target as HTMLElement;
+      if (!clickedElement.classList.contains('mobileLinks') && !clickedElement.classList.contains('mobileMenu') && this.menuShow) {
+        this.toggleMenu();
+      }
+  }
+
   toggleMenu() {
+    this.touched = true;
     this.menuShow = !this.menuShow;
   }
 }
